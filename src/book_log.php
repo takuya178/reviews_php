5533eead48437215d6bac87ea9eddb5fbb50a25c
takuya178 <takuya.u1005@gmail.com>
2022-04-06 07:03:14 +0900

    読書ログを表示するを繰り返し表示

--- src/book_log.php
+++ src/book_log.php
@@ -5,6 +5,7 @@ $author = '';
 $status = '';
 $score = '';
 $summary = '';
+$reviews = [];
 
 while (true) {
     echo '1. 読書ログを登録'. PHP_EOL;
@@ -30,17 +31,28 @@ while (true) {
         
         echo '感想：';
         $summary = trim(fgets(STDIN));
+
+        $reviews[] = [
+            [
+                '書籍名：' => $title,
+                '著者名：' => $author,
+                '読書状況（未読,読んでる,読了）：' => $status,
+                '評価（5点満点の整数）：' => $score,
+                '感想：' => $summary,
+            ]
+        ];
     
         echo '登録が完了しました' . PHP_EOL . PHP_EOL;
     } elseif ($num === '2') {
         // 読書ログを表示
-        echo '登録されている読書ログを表示します' . PHP_EOL;
-        echo '読書ログを表示します' . PHP_EOL;
-        echo '書籍名：' . $title . PHP_EOL;
-        echo '著者名：' . $author . PHP_EOL;
-        echo '読書状況：' . $status . PHP_EOL;
-        echo '評価：' . $score . PHP_EOL;
-        echo '感想：' . $summary . PHP_EOL;
+        foreach ($reviews as $review) {
+            echo '書籍名：' . $review['title'] . PHP_EOL;
+            echo '著者名：' . $review['author'] . PHP_EOL;
+            echo '読書状況：' . $review['status'] . PHP_EOL;
+            echo '評価：' . $review['score'] . PHP_EOL;
+            echo '感想：' . $review['summary'] . PHP_EOL;
+            echo '-------------' . PHP_EOL;
+        }
     } elseif($num === '9') {
         break;
     }
