11da42fa60c1251511480aa474e74dcd65df55ac
takuya178 <takuya.u1005@gmail.com>
2022-04-09 14:19:36 +0900

    ブックログの登録と参照完了

--- src/book_log2.php
+++ src/book_log2.php
@@ -8,12 +8,45 @@
 // 使用しなくなった変数があれば削除しておいてください
 // 確認用に追記した var_export は削除ください
 
-$title = '';
-$author = '';
-$status = '';
-$score = '';
-$summary = '';
-$lists = [];
+// $lists = [];
+// ブックログの登録機能
+function createReview()
+{
+    echo '書籍名：';
+    $title = trim(fgets(STDIN));
+    echo '著者名：';
+    $author = trim(fgets(STDIN));
+    echo '読書状況（未読,読んでる,読了）：';
+    $status = trim(fgets(STDIN));
+    echo '評価（5点満点の整数）：';
+    $score = trim(fgets(STDIN));
+    echo '感想：';
+    $summary = trim(fgets(STDIN));
+    echo '登録が完了しました';
+
+    return [
+        'title' => $title,
+        'author' => $author,
+        'status' => $status,
+        'score' => $score,
+        'summary' => $summary
+    ];
+}
+
+// ブックログの一覧表示
+function reviewList($lists)
+{
+    echo '読書ログを表示します'. PHP_EOL;
+
+    foreach ($lists as $list) {
+          echo '書籍名：' . $list['title'] . PHP_EOL;
+          echo '著者名：' . $list['author'] . PHP_EOL;
+          echo '読書状況（未読,読んでる,読了）：' . $list['status'] . PHP_EOL;
+          echo '評価（5点満点の整数）：' . $list['score'] . PHP_EOL;
+          echo '感想：' . $list['summary'] . PHP_EOL;
+          echo "-------------" . PHP_EOL;
+    }
+}
 
 while(true) {
     echo '読書ログを登録してください'. PHP_EOL;
@@ -23,36 +56,10 @@ while(true) {
     $num = trim(fgets(STDIN));
     
     if ($num === '1') {
-        echo '書籍名：';
-        $title = trim(fgets(STDIN));
-        echo '著者名：';
-        $author = trim(fgets(STDIN));
-        echo '読書状況（未読,読んでる,読了）：';
-        $status = trim(fgets(STDIN));
-        echo '評価（5点満点の整数）：';
-        $score = trim(fgets(STDIN));
-        echo '感想：';
-        $summary = trim(fgets(STDIN));
-        echo '登録が完了しました';
-
-        $lists[] = [
-            'title' => $title,
-            'author' => $author,
-            'status' => $status,
-            'score' => $score,
-            'summary' => $summary
-        ];
+        $lists[] = createReview();
+        var_dump(createReview());
     } elseif ($num === '2') {
-      echo '読書ログを表示します'. PHP_EOL;
-
-      foreach ($lists as $list) {
-            echo '書籍名：' . $list['title'] . PHP_EOL;
-            echo '著者名：' . $list['author'] . PHP_EOL;
-            echo '読書状況（未読,読んでる,読了）：' . $list['status'] . PHP_EOL;
-            echo '評価（5点満点の整数）：' . $list['score'] . PHP_EOL;
-            echo '感想：' . $list['summary'] . PHP_EOL;
-            echo "-------------" . PHP_EOL;
-          }
+        reviewList($lists);
     } elseif ($num === '9') {
         break;
     }
