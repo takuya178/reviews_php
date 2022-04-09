fee1d0f7732ebdca0f3741687ed7ee492028b472
takuya178 <takuya.u1005@gmail.com>
2022-04-09 10:07:20 +0900

    条件分岐で読書ログの登録、表示のプログラム作成

--- src/book_log2.php
+++ src/book_log2.php
@@ -1,35 +1,44 @@
 <?php
-// 読書ログを登録してください
+// 読書ログサービスに、番号2を選択された際に読書ログを表示する処理を記載しましょう。
+// 番号2を選択すると、下記が表示されれば成功です。
+
+// 登録されている読書ログを表示します
 // 書籍名：
 // 著者名：
-// 読書状況（未読,読んでる,読了）：
-// 評価（5点満点の整数）：
+// 読書状況：
+// 評価：
 // 感想：
-// 登録が完了しました
 
-// 読書ログを表示します
-// 書籍名：{登録した書籍名}
-// 著者名：{登録した著者名}
-// 読書状況：{登録した読書状況}
-// 評価：{登録した評価}
-// 感想：{登録した感想}
+$title = '';
+$author = '';
+$status = '';
+$score = '';
+$summary = '';
 
 echo '読書ログを登録してください'. PHP_EOL;
-echo '書籍名：';
-$title = trim(fgets(STDIN));
-echo '著者名：';
-$author = trim(fgets(STDIN));
-echo '読書状況（未読,読んでる,読了）：';
-$status = trim(fgets(STDIN));
-echo '評価（5点満点の整数）：';
-$score = trim(fgets(STDIN));
-echo '感想：';
-$summary = trim(fgets(STDIN));
-echo '登録が完了しました';
+echo '1.読書ログを登録する'. PHP_EOL;
+echo '2.読書ログを表示する'. PHP_EOL;
+echo '9.読書ログを終了する'. PHP_EOL;
+$num = trim(fgets(STDIN));
 
-echo '読書ログを表示します'. PHP_EOL;
-echo '書籍名：' . $title . PHP_EOL;
-echo '著者名：' . $author . PHP_EOL;
-echo '読書状況（未読,読んでる,読了）：' . $status . PHP_EOL;
-echo '評価（5点満点の整数）：' . $score . PHP_EOL;
-echo '感想：' . $summary . PHP_EOL;
+if ($num === '1') {
+    echo '書籍名：';
+    $title = trim(fgets(STDIN));
+    echo '著者名：';
+    $author = trim(fgets(STDIN));
+    echo '読書状況（未読,読んでる,読了）：';
+    $status = trim(fgets(STDIN));
+    echo '評価（5点満点の整数）：';
+    $score = trim(fgets(STDIN));
+    echo '感想：';
+    $summary = trim(fgets(STDIN));
+    echo '登録が完了しました';
+} elseif ($num === '2') {
+    echo '読書ログを表示します'. PHP_EOL;
+    echo '書籍名：' . $title . PHP_EOL;
+    echo '著者名：' . $author . PHP_EOL;
+    echo '読書状況（未読,読んでる,読了）：' . $status . PHP_EOL;
+    echo '評価（5点満点の整数）：' . $score . PHP_EOL;
+    echo '感想：' . $summary . PHP_EOL;
+}
+
