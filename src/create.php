838134ae0aae1d1d1a24ae0e138a775c06f2d570
takuya178 <takuya.u1005@gmail.com>
2022-04-13 08:54:32 +0900

    PHPファイルとHTMLファイルを分割

--- src/create.php
+++ src/create.php
@@ -1,20 +1,4 @@
 <?php
-
-// HTTPメソッドがPOSTだったら
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//     // POSTされた会社情報を変数に格納する
-//     $reviews = [
-//         'title' => $_POST['name'],
-//         'author' => $_POST['author'],
-//         'status' => $_POST['status'],
-//         'score' => $_POST['score'],
-//         'summary' => $_POST['summary'],
-//     ];
-// }
-// バリデーションする
-// データベースにデータを格納する
-// データベースとの接続を切断する
-
 require_once __DIR__ . '/lib/mysqli.php';
 
 function createReview($link, $review)
@@ -103,63 +87,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
     // もしエラーがあれば
 }
-?>
 
-
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>読書ログ登録</title>
-</head>
-
-<body>
-    <h1>読書ログ</h1>
-    <form action="create.php" method="post">
-        <?php if (count($errors)) : ?>
-            <ul>
-                <?php foreach ($errors as $error) : ?>
-                    <li><?php echo $error; ?></li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endif; ?>
-        <div>
-            <label for="title">書籍名</label>
-            <input type="text" name="title" id="title">
-        </div>
-        <div>
-            <label for="author">著者名</label>
-            <input type="text" name="author" id="author">
-        </div>
-        <div>
-            <label>読書状況</label>
-            <div>
-                <div>
-                    <input type="radio" name="status" id="status1" value="未読">
-                    <label for="status1">未読</label>
-                </div>
-                <div>
-                    <input type="radio" name="status" id="status2" value="読んでる">
-                    <label for="status2">読んでる</label>
-                </div>
-                <div>
-                    <input class="form-check-input" type="radio" name="status" id="status3" value="読了">
-                    <label for="status3">読了</label>
-                </div>
-            </div>
-        </div>
-        <div>
-            <label for="score">評価（5点満点の整数）</label>
-            <input type="number" name="score" id="score">
-        </div>
-        <div>
-            <label for="summary">感想</label>
-            <textarea type="text" name="summary" id="summary" rows="10"></textarea>
-        </div>
-        <button type="submit">登録する</button>
-    </form>
-</body>
-</html>
+include 'views/new.php';
